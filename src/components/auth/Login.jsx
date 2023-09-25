cb15e60a38739a6ae2e97b2f83ce99bf95f45fbe
nisarg-frontendarmy <nisarg.frontendarmy@gmail.com>
2023-09-25 19:09:26 +0530

    Redesign-using-React-Bootstrap

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -23,21 +23,44 @@ const Login = () => {
 
   return (
     <div className="d-flex vh-100 main-div1">
-      <div>
+      {/* <div>
         <img
           className="img-fluid d-flex rounded-top-left rounded-bottom-left align-items-center w-100 vh-100"
           src="/images/loginpage.JPG"
           alt="loginpage"
         />
+      </div> */}
+      <div>
+        <img className="main-div8" src="/images/Ellipse1.png" alt="Circle" />
       </div>
-      <div className=" border border-black p-5 flex-column align-items-center main-div2">
-        <div className="text-center fs-2 mb-3 text-black">
+      <div>
+        <img className="main-div9" src="/images/Ellipse2.png" alt="Circle" />
+      </div>
+      <div>
+        <img
+          className="main-div10"
+          src="/images/welcomeback.png"
+          alt="Message..!"
+        />
+        {/* <div className="text-white fs-2 text-left border border-primary main-div11">
+          Glad you are back.!
+        </div> */}
+      </div>
+      {/* <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <div className="fs-1 text-white">Welcome Back.!</div>
+      </div> */}
+      <div className=" border border-white p-5 flex-column align-items-center main-div2">
+        <div className="text-center fs-2 mb-3 text-white">
           Login to your Account
         </div>
         <Form onSubmit={handleSubmit(onSubmit)} className="w-100 m-3">
           <Form.Group controlId="email">
-            <Form.Label className="text-black">Email Address</Form.Label>
+            <Form.Label className="text-white">Email Address</Form.Label>
             <Form.Control
+              className="bg-transparent text-white placeholder-text-color"
               {...register("email", {
                 required: "Please enter email",
                 pattern: {
@@ -57,8 +80,9 @@ const Login = () => {
           </Form.Group>
 
           <Form.Group controlId="password">
-            <Form.Label className="mt-2 text-black">Password</Form.Label>
+            <Form.Label className="mt-2 text-white">Password</Form.Label>
             <Form.Control
+              className="bg-transparent text-white placeholder-text-color"
               {...register("password", {
                 required: "Please enter a password",
                 minLength: {
@@ -75,7 +99,7 @@ const Login = () => {
                 },
               })}
               type="password"
-              placeholder="********"
+              placeholder="Password"
             />
             {errors.password && (
               <span className="text-danger text-left">
@@ -89,7 +113,7 @@ const Login = () => {
               variant="success"
               size="lg"
               type="submit"
-              className="mx-auto d-flex justify-content-center text-black mt-4"
+              className="mx-auto d-flex justify-content-center text-white mt-4"
             >
               Log In
             </Button>
@@ -98,13 +122,13 @@ const Login = () => {
           )}
         </Form>
 
-        <Link className="text-decoration-none  m-2 text-black" to="/signup">
+        <Link className="text-decoration-none  m-2 text-white" to="/signup">
           Don't have an account? Register here.
         </Link>
-        <Link to="/signup" className=" text-decoration-none text-black">
+        <Link to="/signup" className=" text-decoration-none text-white">
           Sign Up
         </Link>
-        <Link to="/forgotpassword" className=" text-decoration-none text-black">
+        <Link to="/forgotpassword" className=" text-decoration-none text-white">
           Forgot Password
         </Link>
       </div>
