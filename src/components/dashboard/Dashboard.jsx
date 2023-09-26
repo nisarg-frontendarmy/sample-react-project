76d447c303a8f8ec82638a489d4354c50eece42a
nisarg-frontendarmy <nisarg.frontendarmy@gmail.com>
2023-09-26 12:22:32 +0530

    Redesign-using-React-bootstrap

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -9,34 +9,36 @@ const Dashboard = () => {
 
   const listItemStyle = {
     backgroundColor: "transparent",
-    border: "1px solid black",
+    border: "1px solid grey",
+    padding: "12px",
+    opacity: "10",
   };
 
   return (
-    <div className="text-center w-50 w-100">
-      <div className="fs-2 mb-4">Welcome to Students Lists Page</div>
+    <div className="text-center w-100 vh-100 main-div19">
+      <div className="fs-2 mb-4 text-white">Welcome to Students Lists Page</div>
       <Table className="w-100">
         <thead>
-          <tr className="bg-dark">
-            <th className="text-white" style={listItemStyle}>Student Id</th>
-            <th className="text-white" style={listItemStyle}>Student Name</th>
-            <th className="text-white" style={listItemStyle}>Department</th>
-            <th className="text-white" style={listItemStyle}>Course Name</th>
-            <th className="text-white" style={listItemStyle}>Name Of Institute</th>
-            <th className="text-white" style={listItemStyle}>View Full Details</th>
+          <tr className="w-5 mt-10">
+            <th className="text-white bg-secondary" style={listItemStyle}>Student Id</th>
+            <th className="text-white bg-secondary" style={listItemStyle}>Student Name</th>
+            <th className="text-white bg-secondary" style={listItemStyle}>Department</th>
+            <th className="text-white bg-secondary" style={listItemStyle}>Course Name</th>
+            <th className="text-white bg-secondary" style={listItemStyle}>Name Of Institute</th>
+            <th className="text-white bg-secondary" style={listItemStyle}>View Full Details</th>
           </tr>
         </thead>
         <tbody>
           {JSON.Dashboard.map((res, index) => (
             <tr key={index}>
-              <td style={listItemStyle}>{res.studentId}</td>
-              <td style={listItemStyle}>{res.studentName}</td>
-              <td style={listItemStyle}>{res.department}</td>
-              <td style={listItemStyle}>{res.courseName}</td>
-              <td style={listItemStyle}>{res.instituteName}</td>
-              <td style={listItemStyle}>
+              <td className="text-white"  style={listItemStyle}>{res.studentId}</td>
+              <td className="text-white"  style={listItemStyle}>{res.studentName}</td>
+              <td className="text-white"  style={listItemStyle}>{res.department}</td>
+              <td className="text-white"  style={listItemStyle}>{res.courseName}</td>
+              <td className="text-white"  style={listItemStyle}>{res.instituteName}</td>
+              <td className="text-white"  style={listItemStyle}>
                 <Button
-                  variant="success"
+                  variant="secondary"
                   className="mx-auto d-flex justify-content-center"
                   onClick={() => history(`/SingleStudent?${res.studentId}`)}
                 >
