2b34717eeeeaba0e48989a59600aa1f255b6e857
nisarg-frontendarmy <nisarg.frontendarmy@gmail.com>
2023-10-11 17:50:06 +0530

    React

--- src/components/dashboard/SingleStudent.jsx
+++ src/components/dashboard/SingleStudent.jsx
@@ -2,13 +2,13 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchSingleStudentAsync } from "../../features/slice/studentslice";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, Image } from "react-bootstrap";
 
 const SingleStudent = () => {
   const { studentId } = useParams();
   const dispatch = useDispatch();
-  const {singleStudent} = useSelector((state) => state.singleStudent);
-  console.log("singleStudent",singleStudent)
+  const { singleStudent } = useSelector((state) => state.singleStudent);
+  console.log("singleStudent", singleStudent);
 
   useEffect(() => {
     dispatch(fetchSingleStudentAsync(studentId));
@@ -20,7 +20,9 @@ const SingleStudent = () => {
 
   return (
     <div className="border border-black w-50 m-5">
-      <div className="text-left p-2 fs-2 text-white bg-dark">Student Information</div>
+      <div className="text-left p-2 fs-2 text-white bg-dark">
+        Student Information
+      </div>
       <ListGroup>
         <ListGroup.Item className="text-black border-bottom-0">
           ID : {singleStudent.id}
@@ -34,7 +36,11 @@ const SingleStudent = () => {
         <ListGroup.Item className="text-black border-bottom-0">
           Email: {singleStudent.email}
         </ListGroup.Item>
-        <ListGroup.Item className="text-black border-bottom-0">Avatar: {singleStudent.avatar}</ListGroup.Item>
+        <div className="main-Image">
+          <ListGroup.Item className="text-black border-bottom-0 ">
+            <Image src={singleStudent.avatar} alt="Student Avatar"/>
+          </ListGroup.Item>
+        </div>
       </ListGroup>
     </div>
   );
